c9778fd7a8b9a9ad5ddfeddc00081e5fb78d7ce0
itprogaur <professionalcodehub@gmail.com>
2022-01-05 21:27:21 +0530

    Code modefied

--- src/App.js
+++ src/App.js
@@ -11,22 +11,33 @@ class App extends Component() {
        monsters : []
      };
   }
-}
+  
 
   componentDidMount(){
+      try {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(users => this.setState({monsters:users}));
-  }
+    
+    .then(users =() => {
+      console.log(response); 
+      this.setState({monsters:users}));
+    }
+    
+} catch(err){
 
+  console.log(err);
+  
+}
+  }
   render(){
     return(
       <div className="App">
         {this.state.monsters.map(monster => (
-          <h1>key={monster.id} {monster.name}</h1>
+          <h1 key={monster.id}> {monster.name}</h1>
         ))}
       </div>
     );
   }
+}
 
 export default App;
